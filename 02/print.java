6429799431d8bb7785f124cca7faa4319f6565c2
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-04-29 09:41:33 +0530

    Print basics

---
+++ 02/print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class print{
+private static Scanner kh;
+	public static void main(String[] args) {
+ kh = new Scanner(System.in);
+		
+System.out.println("Who thou be ?");
+System.out.println(kh.nextLine());
+
+System.out.println("I'll teach you something about printing");
+System.out.print("This is the next line");
+System.out.println("		but this isn't");
+	}
+
+}
