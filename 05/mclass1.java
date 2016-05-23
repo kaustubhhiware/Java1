7f22fbcb0bfe06b8b44f2aadf02d527061be3a92
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-05-23 13:10:28 +0530

    Trivial class example

---
+++ 05/mclass1.java
@@ -0,0 +1,10 @@
+
+public class mclass1 {
+	public static void main(String args[])
+	{
+		tuna Tuna = new tuna();
+		Tuna.mess();
+		
+	}
+
+}
---
+++ 05/tuna.java
@@ -0,0 +1,9 @@
+
+public class tuna {
+	public void mess(){
+	
+	System.out.println("ye hai Dusra class");
+	System.out.println("this is another class");
+
+	}
+}
