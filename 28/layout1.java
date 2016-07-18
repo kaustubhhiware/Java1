c97a1e9f6552382a3e4f779cc4485f3a20784acc
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-07-18 21:46:54 +0530

    Start GUI ...layouts

---
+++ 28/layout1.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class layout1 {
+
+public static void main(String args[]){
+
+		layout2 go= new layout2();	
+		
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(300,200);
+		go.setVisible(true);
+		
+	}
+	
+	
+}
